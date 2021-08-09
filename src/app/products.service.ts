99d73a870ef7a20e59fb88a7cec0221c46fd448f
hugo <hugo.ayre@gmail.com>
2021-08-09 12:47:30 +1200

    product service now uses the Product model

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from "@angular/core";
-
+import { Product } from "./products/products.model";
 @Injectable({providedIn: 'root'})
 export class ProductsService {
-    private products: any[] = [{name: 'Sledgehammer',price: 125.75},{name: 'Axe',price: 190.50},{name: 'Bandsaw',price: 562.13},{name: 'Chisel',price: 12.9},{name: 'Hacksaw',price: 18.45}];
+    private products: Product[] = [{name: 'Sledgehammer',price: 125.75},{name: 'Axe',price: 190.50},{name: 'Bandsaw',price: 562.13},{name: 'Chisel',price: 12.9},{name: 'Hacksaw',price: 18.45}];
 
     getProducts(){
         return [...this.products]
